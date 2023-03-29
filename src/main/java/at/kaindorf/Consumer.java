642b22d5b9e3ef52b04f37f996d443e9167791c8
simon <simon.schoeggler@hotmail.com>
2023-03-29 09:19:46 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/Consumer.java
@@ -0,0 +1,11 @@
+package at.kaindorf;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> Threads<br>
+* <b>User:</b> Simon Schoeggler<br>
+* <b>Date:</b> 29. März 2023<br>
+* <b>Time:</b> 09:01<br>
+*/
+
+public class Consumer {
+}
